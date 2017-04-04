9fdf3a54542bc39740668eb92ac93945721394a5
li gang <lg.careercross@gmail.com>
2017-04-04 09:05:43 +0900

    formal submit

--- ass1/app.js
+++ ass1/app.js
@@ -2,14 +2,25 @@
     'use strict';
 
     angular.module('LunchCheck', [])
+    .controller('LunchCheckController',LunchCheckController);
+    LunchCheckController.$inject=['$scope'];
+    function LunchCheckController ($scope) {
+        $scope.test = "hello";
+        $scope.meals = "";
+        $scope.check = function (meals) {
+            var res = meals.split(",");
+            $scope.colorflg = false;
+            if (res==""){
+                $scope.message="Enter the data!";
+                $scope.colorflg = true;
+                return;
+            }
+            if (res.length > 3) {
+                $scope.message = "Too much!";
+            }else{
+                $scope.message = "Enjoy!";}
+            }
 
-    .controller('LunchCheckController', LunchCheckController);
-    LunchCheckController.$inject = ['$scope'];
-    function LunchCheckController($scope) {
-        $scope.check = function () {
-            alert("Hello!");
-        }
-
-    }
+    };
 
 })();
